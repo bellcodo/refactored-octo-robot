73833e3159eb6e4c5b336161ed9cc33b3bb12a41
Michael Schober <mschober@expedia.com>
2016-04-22 08:48:02 -0700

    removing debug

--- web_world.py
+++ web_world.py
@@ -40,7 +40,6 @@ if __name__ == "__main__":
     # go get the PORT from the environment
     port = os.environ.get("PORT")
     # run the app with the port and bind to any ip
-    app.debug = True
     app.run(
       "0.0.0.0"
     , port
