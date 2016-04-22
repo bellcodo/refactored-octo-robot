8ebee748f9877a212bd87823ee5b00dddbfcefe0
Bell Codo <bellcodo@gmail.com>
2016-04-22 08:16:08 -0700

    Update web_world.py

--- web_world.py
+++ web_world.py
@@ -2,8 +2,7 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+def get_choice():
     # import the magic random picker
     import random, urllib2
     
@@ -27,30 +26,13 @@ def hello():
     """ % (url_for_list, typing_choice)
     return todays_choice
 
-@app.route('/activity_picker')
+@app.route("/")
+def hello():
+    return get_choice()
+
+@app.route('/activity_picker', methods=["GET"])
 def show_entries():
-    # import the magic random picker
-    import random, urllib2
-    
-    # list of things to pick from
-    url_for_list = "https://raw.githubusercontent.com/bellcodo/laughing-meme/master/typing_options.lst"
-    raw_typing_options = urllib2.urlopen(url_for_list)
-    list_of_options = raw_typing_options.read()
-    #print "DEBUG - raw typing options is %s, %s" % (raw_typing_options, list_of_options)
-    typing_options = list_of_options.split()
-    
-    # find a way to pick something
-    typing_choice = random.choice(typing_options)
-    
-    # a way to tell the user what to do
-    print """This is the typing program picker.
-    It will pick a random typing_choice from a list.
-    The list is kept here %s
-    Todays typing choice is...
-    
-    %s
-    """ % (url_for_list, typing_choice)
-    typing_url = typing_choice
+    typing_url = get_choice()
     return render_template('show_entries.html', typing_ul=typing_url)
 
 if __name__ == "__main__":
